bebbc5cc3b155f9fdccb2a753b33b4977105a3be
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-02-13 15:26:50 +1100

    Added Timer
    Added timer script

--- Assets/Scripts/RandomBtnImage.cs
+++ Assets/Scripts/RandomBtnImage.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class RandomBtnImage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //this code is made to set a random picture to the buttons
+    //if the button is the same, call the deactivate function from toolkit (namespace)
+    //place this on the empty parent
+    //button variable
+    //sprite variable
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //method to change image on button, add an array?? and make it choose
+    //a random number from that array
 }
