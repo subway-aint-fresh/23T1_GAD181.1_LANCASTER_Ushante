ad5d4c54b667e05e7cfa8231cb557f9d4c46e37f
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-02-14 03:47:29 +1100

    Added app files to apps prefabs

--- Assets/Scripts/AppController.cs
+++ Assets/Scripts/AppController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemController : MonoBehaviour
+public class AppController : MonoBehaviour
 {
     public App App;
 }
