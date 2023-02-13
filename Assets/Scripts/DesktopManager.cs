9a6188bb794bff99dddd77b454766e303a2425f4
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-02-14 05:00:23 +1100

    Adding Images and text to apps
    Putting image and names under desktop apps

--- Assets/Scripts/DesktopManager.cs
+++ Assets/Scripts/DesktopManager.cs
@@ -1,26 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class DesktopManager : MonoBehaviour
-{
-    public static DesktopManager Instance;
-    public List<App> Apps = new List<App>();
+//public class DesktopManager : MonoBehaviour
+//{
+//    public static DesktopManager Instance;
+//    public List<App> Apps = new List<App>();
 
-    private void Awake()
-    {
-        Instance = this;
-    }
+//    public GameObject DesktopAppSample;
+//    public Transform AppContent;
 
-    //method to add apps to desktop
-    public void Add(App app)
-    {
-        Apps.Add(app);
-    }
+//    private void Awake()
+//    {
+//        Instance = this;
+//    }
 
-    //method to remove apps from desktop
-    public void Remove(App app)
-    {
-        Apps.Remove(app);
-    }
-}
+//    //method to add apps to desktop
+//    public void Add(App app)
+//    {
+//        Apps.Add(app);
+//    }
+
+//    //method to remove apps from desktop
+//    public void Remove(App app)
+//    {
+//        Apps.Remove(app);
+//    }
+
+
+//    void start()
+//    {
+//        GameObject obj = Instantiate(DesktopAppSample, AppContent);
+//        var appName = obj.transform.Find(App / AppName).GetComponent<Text>();
+//        var appIcon = obj.transform.Find(App / AppIcon).GetComponent<Text>();
+
+//        appName.text = app.appName;
+//        appIcon.sprite = app.icon;
+//    }
+//}
