b15c77e48843049bcee265efd7b96e6adfb59025
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-02-13 17:33:27 +1100

    Win screen
    - edited script to display a win screen
    - added win screen + button

--- Assets/Scripts/DeactivateIcon.cs
+++ Assets/Scripts/DeactivateIcon.cs
@@ -1,22 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace Toolkit.ObjectFound
 {
     public class DeactivateIcon : MonoBehaviour
     {
-        //image variable for icon
-        public GameObject icon;
+        //variable for icon screen buttons
+        //private GameObject icon;
+
+
+        //win screen image
+        public Image winScreen;
+
+        //score
+        private int currentPoints;
+        private int pointsToWin;
+
+        private void Start()
+        {
+            pointsToWin = 6;
+        }
+
+        private void Update()
+        {
+            if (currentPoints >= pointsToWin)
+            {
+                //end game, show win screen
+                //stop timer
+                winScreen.enabled = true;
+
+            }
+        }
 
         //method that deactivates icon on click
+
         public void IconDeactivation()
         {
             Debug.Log("clicked");
-            icon.SetActive(false);
+            //icon.SetActive(false);
             Destroy(gameObject);
+            currentPoints++;
         }
+
     }
 }
 
