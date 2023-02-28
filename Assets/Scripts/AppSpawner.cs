b8ffdfe1dd26254cf33a6b7baa7eb5e5380a1b1a
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-02-28 14:04:10 +1100

    Initial code app spawner script
    created code that instantiates the github prefab on start, and assigns it as the child of the 'content' game object.

--- Assets/Scripts/AppSpawner.cs
+++ Assets/Scripts/AppSpawner.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class AppSpawner : MonoBehaviour
 {
+    //github prefab (change to an array of apps later)
+    public GameObject github;
+
+    //content folder
+    public GameObject content;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject go = Instantiate(github, new Vector3 (0,0,0), Quaternion.identity) as GameObject; 
+        go.transform.SetParent(GameObject.Find("Content").transform);
     }
 
     // Update is called once per frame
