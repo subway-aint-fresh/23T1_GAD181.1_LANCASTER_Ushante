65834c2d24447f74f4fbe0484b8c2d75e36b598d
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-02-28 14:59:24 +1100

    App spawner script
    altered script to instantiate all 'app' prefabs with the correct proportions.

--- Assets/Scripts/AppSpawner.cs
+++ Assets/Scripts/AppSpawner.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class AppSpawner : MonoBehaviour
 {
-    //github prefab (change to an array of apps later)
-    public GameObject github;
-
-    //content folder
-    public GameObject content;
+    public GameObject[] appsArray = new GameObject[44];
 
     // Start is called before the first frame update
     void Start()
-    {
-        GameObject go = Instantiate(github, new Vector3 (0,0,0), Quaternion.identity) as GameObject; 
-        go.transform.SetParent(GameObject.Find("Content").transform);
+    {   
+
+        for ( int i = 0; i < 44; i++)
+        {
+            appsArray[i] = Instantiate(appsArray[i], new Vector3 (1,1,1), Quaternion.identity) as GameObject; 
+            appsArray[i].transform.SetParent(GameObject.Find("Content").transform, false);
+            
+        }
+
     }
 
     // Update is called once per frame
