813635b9bd4e4fa9b15dc8d0640c3c937a64e551
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-02-28 15:15:52 +1100

    App spawner script
    Apps now spawn in a random order on start

--- Assets/Scripts/AppSpawner.cs
+++ Assets/Scripts/AppSpawner.cs
@@ -10,11 +10,20 @@ public class AppSpawner : MonoBehaviour
     void Start()
     {   
 
+        //make the array randomized
+
         for ( int i = 0; i < 44; i++)
         {
+            for (int t = 0; t < appsArray.Length; t++ )
+            {
+                GameObject tmp = appsArray[t];
+                int r = Random.Range(t, appsArray.Length);
+                appsArray[t] = appsArray[r];
+                appsArray[r] = tmp;
+            }
             appsArray[i] = Instantiate(appsArray[i], new Vector3 (1,1,1), Quaternion.identity) as GameObject; 
             appsArray[i].transform.SetParent(GameObject.Find("Content").transform, false);
-            
+
         }
 
     }
@@ -24,4 +33,5 @@ public class AppSpawner : MonoBehaviour
     {
         
     }
+
 }
